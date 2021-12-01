f2475e545154310d4a2b1415ff434c2075a921f4
Puleng Mtambo <mtambo.ap@gmail.com>
2021-12-01 23:35:20 +0200

    Elseif Tutorial

---
+++ Else if Tutorial.py
@@ -0,0 +1,8 @@
+x = input('Name: ')
+
+if x == 'Puleng':
+     print('You are a TechUp School student')
+elif x == 'Nicol':
+     print("You are Puleng's mentor")
+else:
+    print('Sorry! The system does not recognize you.')
